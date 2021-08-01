a3f4410b12b1ba59906bfb78426d947d44ecf4c3
startaiyo <startaiyo0104@gmail.com>
2021-08-01 22:45:53 +0900

    Initial Commit

---
+++ Eput/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Eput
+//
+//  Created by 土井星太朗 on 2021/08/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Eput/EputApp.swift
@@ -0,0 +1,17 @@
+//
+//  EputApp.swift
+//  Eput
+//
+//  Created by 土井星太朗 on 2021/08/01.
+//
+
+import SwiftUI
+
+@main
+struct EputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
