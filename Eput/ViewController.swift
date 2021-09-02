a4dc629415a4a3a1331d8a911d492abd07d8bf02
startaiyo <startaiyo0104@gmail.com>
2021-09-02 22:23:16 +0900

    画面遷移

--- Eput/ViewController.swift
+++ Eput/ViewController.swift
@@ -6,13 +6,19 @@ class InputList: Object{
     @objc dynamic var content : String = ""
 }
 class NewInputViewController: UIViewController{
+    @IBOutlet weak var backbutton: UIButton!
     @IBOutlet weak var txtContent: UITextField!
     private let realm = try! Realm()
     public var completionHandler: (() -> Void)?
     override func viewDidLoad() {
         super .viewDidLoad()
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(btnSave))
+        backbutton.frame = CGRect(x: 0, y: 100, width: 300, height: 30)
+        backbutton.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
     }
+    @objc func didTapButton() {
+        self.dismiss(animated: true, completion: nil)
+        }
     @objc func btnSave(_ sender: Any){
         let inputContent = txtContent.text
         realm.beginWrite()
@@ -35,6 +41,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         let url: URL = URL(string: "https://fw79fhpu20.execute-api.ap-northeast-1.amazonaws.com/default/cssappfunc/cssdetail/1")!
         let task: URLSessionTask = URLSession.shared.dataTask(with: url, completionHandler: {data, response, error in
             print("data: \(String(describing: data))")
@@ -42,6 +49,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             print("error: \(String(describing: error))")
         })
         task.resume() //実行する
+
         input = realm.objects(InputList.self).map({$0})
         let hoge = "ホゲホゲホゲホゲホゲホゲピヨピヨピヨピヨピヨ"
         initView(i: hoge)
@@ -88,8 +96,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         button.setTitle("テキスト読み上げ", for: .normal)
         button.addTarget(self, action: #selector(ViewController.speech), for: .touchUpInside)
         self.view.addSubview(button)
+        inputbutton.frame = CGRect(x: 0, y: 100, width: 300, height: 30)
+        inputbutton.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
     }
+    @objc func didTapButton() {
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "NewInput") as! NewInputViewController
+        self.present(vc, animated: true, completion: nil)
 
+        }
     @objc func speech(){
         let speechSynthesizer = AVSpeechSynthesizer()
         let utterance = AVSpeechUtterance(string: self.label.text!)
