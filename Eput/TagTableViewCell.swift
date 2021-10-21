306f15c40f5581e109d7d8e45115ed1f63d5b285
startaiyo <startaiyo0104@gmail.com>
2021-10-22 07:58:14 +0900

    tagview追加

---
+++ Eput/TagTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TagTableViewCell.swift
+//  Eput
+//
+//  Created by 土井星太朗 on 2021/10/21.
+//
+
+import UIKit
+
+class TagTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
