4846f09d6db3dafe5c91bbd054eba8660777e5e2
startaiyo <startaiyo0104@gmail.com>
2021-10-25 08:28:12 +0900

    menubar追加

---
+++ Eput/ContentsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ContentsViewController.swift
+//  Eput
+//
+//  Created by 土井星太朗 on 2021/10/25.
+//
+
+import UIKit
+
+class ContentsViewController: UIViewController {
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+            super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        }
+
+        required init?(coder aDecoder: NSCoder) {
+            super.init(coder: aDecoder)
+        }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
